4343f1a197ab725931cf3ccb6ce328794e7223ab
Micha Davis <78385236+Micha-L-Davis@users.noreply.github.com>
2022-04-04 16:40:23 -0700

    Update index.js

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -5,7 +5,12 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite::memory';
 
-const sequelize = new Sequelize(DATABASE_URL);
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: true,
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = {
   authDb: sequelize,
